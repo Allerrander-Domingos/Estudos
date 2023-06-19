ddb8e056252d9052d336ea70005d9d1c262b0073
Allerrander Domingos <allerranderdomingos1@gmail.com>
2023-06-19 19:19:56 -0300

    ds

--- Nautta/scrtpt.js
+++ Nautta/scrtpt.js
@@ -1,9 +1,6 @@
 var btn = document.querySelector('.categorias')
 var functions = document.querySelector('.functions')
 
-var burguer = document.querySelector('.burguer')
-var close = document.querySelector('.close')
-
 btn.addEventListener('click', function() {
 
     if(functions.style.display === 'block') {
@@ -17,22 +14,56 @@ btn.addEventListener('click', function() {
     }
 });
 
+btn.addEventListener('click', function() {
+
+    if(beleza.style.display === 'block') {
+    beleza.style.display = 'none';
+    }
+});
 
 
+
+var beleza = window.document.querySelector('.beleza')
+var infantil = window.document.querySelector('.infantil')
+var masculino = window.document.querySelector('.masculino')
 var feminino = window.document.querySelector('.feminino')
-var n4 = window.document.querySelector
-('.n4')
-var fem = window.document.querySelector('.fem')
+var esporte = window.document.querySelector('.esporte')
 
-n4.addEventListener('mouseenter', entrar)
+var n1 = window.document.querySelector('.n1') 
+var n2 = window.document.querySelector('.n2')
+var n3 = window.document.querySelector('.n3')
+var n4 = window.document.querySelector('.n4')
+var n5 = window.document.querySelector('.n5')
+
+n1.addEventListener('mouseenter', entrar1)
+n2.addEventListener('mouseenter', entrar2)
+n3.addEventListener('mouseenter', entrar3)
+n4.addEventListener('mouseenter', entrar4)
+n5.addEventListener('mouseenter', entrar5)
+
+beleza.addEventListener('mouseout', sair)
+infantil.addEventListener('mouseout', sair)
+masculino.addEventListener('mouseout', sair)
 feminino.addEventListener('mouseout', sair)
+esporte.addEventListener('mouseout', sair)
 
-function entrar() {
-    feminino.style.display = 'block'
-    
+function entrar1() {
+    beleza.style.display = 'block'
+
 }
 
-function sair() {
-    feminino.style.display = 'block'
+function entrar2() {
+    infantl.style.display = 'block'
+}
 
+function entrar3() {
+    masculino.style.display = 'block'
+}
+
+function entrar4() {
+    feminino.style.display = 'block'
+}
+
+function entrar5() {
+    esporte.style.display = 'block'
 }
